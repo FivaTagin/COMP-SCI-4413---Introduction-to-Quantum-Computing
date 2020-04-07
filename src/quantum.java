a4350d045c41055b30528f2dd20494a9ccba1091
FivaTagin <fiva1987@gmail.com>
2020-04-08 08:36:10 +0930

    08042020 - Log Data to Array

--- src/quantum.java
+++ src/quantum.java
@@ -7,23 +7,37 @@ import java.util.Arrays;
 import java.util.*;
 
 class quantum {
+    
+    //
+    // Private 
+    //
 
+        //
+        // Private global variables
+        //
+    private static int INDEX_T = 0; // the index of T colum
+    private static int INDEX_M = 1; // the index of nput quantum states to process.
+    private static int INDEX_WIRE_1 = 2; // the index of Wire 1
+    private static int INDEX_WIRE_2 = 3; // the index of Wire 2
+        
+    //
+    // Public 
+    //
     public static void main (String[] strs)  throws FileNotFoundException, IOException{
-        List<Integer> list = new ArrayList<Integer>();
+        List<String> list = new ArrayList<String>();
         File file = new File(strs[0]);
-        BufferedReader reader = null;
+        int varIndex = 0;
         Scanner scanner = new Scanner(file);
         
         System.out.println("quantum");
 
+        // obstrate the data from input file.
         while(scanner.hasNext()){                                             
-            System.out.println(scanner.next());                            
+            list.add(varIndex, scanner.next());
+            varIndex++;                
         }
 
         
-        // List<Integer> ints = Files.lines(Paths.get(file))
-        // .map(Integer::parseInt)
-        // .collect(Collectors.toList());
 
         //print out the list
         System.out.println(list);
