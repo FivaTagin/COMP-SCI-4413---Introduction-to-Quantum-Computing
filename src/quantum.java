52d3be804b3989ef7b99c79bc871d2b9f81d6c6f
FivaTagin <fiva1987@gmail.com>
2020-04-08 07:57:44 +0930

    08042020- First Edition for empty structure
    the first structure.
    
    functionality : read files from designated target.
    
    Non Error

--- src/quantum.java
+++ src/quantum.java
@@ -1,7 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.String;
+import java.util.Arrays;
+
+
+import java.util.*;
+
 class quantum {
 
-    public static void main (String[] strs) {
+    public static void main (String[] strs)  throws FileNotFoundException, IOException{
+        List<Integer> list = new ArrayList<Integer>();
+        File file = new File(strs[0]);
+        BufferedReader reader = null;
+        Scanner scanner = new Scanner(file);
+        
         System.out.println("quantum");
+
+        while(scanner.hasNext()){                                             
+            System.out.println(scanner.next());                            
+        }
+
+        
+        // List<Integer> ints = Files.lines(Paths.get(file))
+        // .map(Integer::parseInt)
+        // .collect(Collectors.toList());
+
+        //print out the list
+        System.out.println(list);
+        
+        
+        scanner.close();
         return;
     }
 }
